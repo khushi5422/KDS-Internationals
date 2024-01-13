3f0ab64c6998b0ff517b9cb3f8c05ee620ee381f
khushi5422 <khushipatel6476@gmail.com>
2024-01-13 10:45:19 +0530

    images changed in parallax

--- src/components/imageByIndex.tsx
+++ src/components/imageByIndex.tsx
@@ -1,10 +1,13 @@
 export const images: string[] = [
-    '/corianderseeds.png',
-    '/fennelseeds.png',
-    '/greenpeppercorns.png',
-    '/rosehip.png',
-    '/saffron.png',
-    '/sesme.png'
+  "blacksessme.png",
+  "chilly.png",
+  "cinnamon.png",
+  "elaichi.png",
+  "fenugreek.png",
+  "pimento.png",
+  "saffron.png",
+  "taj.png",
+  "Fennelseeds.png"
   ];
   
   const imageByIndex = (index: number): string => images[index % images.length];
